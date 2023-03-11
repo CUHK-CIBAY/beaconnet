47f9c5491d93ea5f7cdd1a8fe83a3d5082d61112
commitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-03-11 19:47:00 +0800

    refactor: move mock data to same file to maintain single source of data

--- graphql-gateway/js-graphql/src/resolvers/bit-impl.ts
+++ graphql-gateway/js-graphql/src/resolvers/bit-impl.ts
@@ -1,22 +1,4 @@
-import { Bit } from '../gql.types';
-
-// Mock data
-export const bits: Bit[] = [
-    {
-        id: '1',
-        content: 'hello world',
-        createAt: '2023-03-10T19:38:56.179Z',
-        totalLike: 0,
-        likeGiversId: []
-    },
-    {
-        id: '2',
-        content: 'So sleepy',
-        createAt: '2023-03-10T19:40:56.179Z',
-        totalLike: 1,
-        likeGiversId: ['1']
-    }
-]
+import { bits } from '../mock-data';
 
 const resolvers = {
     Query: {
--- graphql-gateway/js-graphql/src/resolvers/user-impl.ts
+++ graphql-gateway/js-graphql/src/resolvers/user-impl.ts
@@ -1,46 +1,5 @@
-import { Gender, User, UserInfo, Bit } from '../gql.types';
-
-// Mock data
-const users: User[] = [
-    {
-        id: '1', 
-        email: 'test@test.com', 
-        nickname: null,
-        followingIds: [],
-        username: 'test',
-        password: '123456',
-        info: {gender: Gender.HIDDEN},
-        bitsId: null,
-    },
-    {
-        id: '2',
-        email: 'test2@test.com',
-        nickname: 'Alex',
-        followingIds: ['1'],
-        username: 'TEST2',
-        password: '123456',
-        info: {gender: Gender.MALE},
-        bitsId: ['1','2'],
-    }
-]
-
-const bits: Bit[] = [
-    {
-        id: '1',
-        content: 'hello world',
-        createAt: '2023-03-10T19:38:56.179Z',
-        totalLike: 0,
-        likeGiversId: []
-    },
-    {
-        id: '2',
-        content: 'So sleepy',
-        createAt: '2023-03-10T19:40:56.179Z',
-        totalLike: 1,
-        likeGiversId: ['1']
-    }
-]
-
+import { User, UserInfo } from '../gql.types';
+import { users, bits } from '../mock-data';
 
 // helper
 const filterUsersByUserIds = (userIds: string[]) => 
@@ -61,19 +20,7 @@ const resolvers = {
         bits: (p: User) => findBitsByBitIds(p.bitsId ?? []) 
     },
     UserInfo: {
-        gender: (p: UserInfo) => {
-            return p.gender;
-            switch (p.gender) {
-                case Gender.MALE:
-                    return 'MALE';
-                case Gender.FEMALE:
-                    return 'FEMALE';
-                case Gender.OTHER:
-                    return 'OTHER';
-                case Gender.HIDDEN:
-                    return 'HIDDEN';
-            }
-        }
+        gender: (p: UserInfo) => p.gender,
     }
             
 };
