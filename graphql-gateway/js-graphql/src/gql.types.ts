6925a24ad1e9246f773b44ec8549334fb6128d75
commitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-03-11 18:04:22 +0800

    feat: add graphql code generator for auto type definition

---
+++ graphql-gateway/js-graphql/src/gql.types.ts
@@ -0,0 +1,62 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Bit = {
+  __typename?: 'Bit';
+  content: Scalars['String'];
+  /** Date format in ISO8601 */
+  createAt: Scalars['String'];
+  id: Scalars['ID'];
+  likeGivers?: Maybe<Array<User>>;
+  totalLike?: Maybe<Scalars['Int']>;
+};
+
+export enum Gender {
+  Female = 'FEMALE',
+  Hidden = 'HIDDEN',
+  Male = 'MALE',
+  Other = 'OTHER'
+}
+
+export type Info = {
+  __typename?: 'Info';
+  gender?: Maybe<Gender>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  bit?: Maybe<Bit>;
+  bits?: Maybe<Array<Bit>>;
+  /** Simple check about gateway connectivity, return OK */
+  healthcheck: Scalars['String'];
+  /** Get current User */
+  me?: Maybe<User>;
+  user?: Maybe<User>;
+  users?: Maybe<Array<User>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  bits: Array<Maybe<Bit>>;
+  email: Scalars['String'];
+  following?: Maybe<Array<User>>;
+  id: Scalars['ID'];
+  info?: Maybe<Info>;
+  /** User custom name, can duplicate */
+  nickname?: Maybe<Scalars['String']>;
+  /** Hashed password */
+  password: Scalars['String'];
+  /** Use for login and identify, unique */
+  username: Scalars['String'];
+};
