13fe9248b2522f8ca07265ac92d94407736aab91
commitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-03-26 04:28:54 +0800

    chore: unify error message

--- graphql-gateway/js-graphql/src/resolvers/user/register.ts
+++ graphql-gateway/js-graphql/src/resolvers/user/register.ts
@@ -17,12 +17,7 @@ const userRegisterResolver = async (_p: any, { input }: MutationRegisterArgs) =>
         const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
         let query = `MATCH (u:User) WHERE u.username = $username OR u.email = $email RETURN u`
         let result = await session.run(query, {username, email});
-        if(result.records.length > 0) {
-            throw {
-                username: 'username already in use',
-                status: 400
-            }
-        }
+        if(result.records.length > 0) throw Error('username or email is used');
         result = await session.run('MATCH (:User) WITH toString(COUNT(*) + 1) as count CREATE (u:User {id: count, username: $username, password: $password, email: $email}) RETURN u', { username, password: hashedPassword, email });
         return result.records[0].get('u').properties;
     } catch (error) {
