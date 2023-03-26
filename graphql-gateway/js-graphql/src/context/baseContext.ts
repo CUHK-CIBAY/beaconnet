0b5ebb22a1045e78bfcb68c6b0e220db0723a88d
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-03-27 03:01:18 +0800

    feat: move database driver into context (#19)
    * refactor: separate the context out
    
    * refactor: move database driver into context

---
+++ graphql-gateway/js-graphql/src/context/baseContext.ts
@@ -0,0 +1,26 @@
+import neo4j from 'neo4j-driver';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const jwt = require('jsonwebtoken');
+
+const driver = neo4j.driver(
+    process.env.DB_URL ?? '',
+    neo4j.auth.basic(process.env.DB_USER ?? '', process.env.DB_PASSWORD ?? ''),
+);
+
+const baseContext = async ({ req }: any) => {
+    const token = req.headers['x-token'];
+    if (token) {
+        try {
+            const me = await jwt.verify(token, process.env.SERCET);
+            return{ me, driver };
+        } catch (error) {
+            throw new Error('Your session expired. Please sign in again.');
+        }
+    }
+    return { driver };
+}
+
+export default baseContext;
--- graphql-gateway/js-graphql/src/index.ts
+++ graphql-gateway/js-graphql/src/index.ts
@@ -3,26 +3,13 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schemas';
 import resolvers from './resolvers';
 
-const jwt = require('jsonwebtoken');
-
-const baseContext = async ({ req }: any) => {
-    const token = req.headers['x-token'];
-    if (token) {
-        try {
-            const me = await jwt.verify(token, process.env.SERCET);
-            return { me };
-        } catch (error) {
-            throw new Error('Your session expired. Please sign in again.');
-        }
-    }
-    return {};
-};
+import baseContext from './context/baseContext';
 
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs: await typeDefs,
     resolvers,
-    context: await baseContext,
+    context: baseContext,
   });
 
   const { url } = await server.listen();
