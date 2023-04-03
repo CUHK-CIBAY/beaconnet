d7640077d8f3d9943ba601f9639eb6500fb2a056
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-04-04 02:53:54 +0800

    chore: remove unncessary comment

--- graphql-gateway/js-graphql/src/context/baseContext.ts
+++ graphql-gateway/js-graphql/src/context/baseContext.ts
@@ -1,14 +1,8 @@
-// import neo4j from 'neo4j-driver';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const jwt = require('jsonwebtoken');
-
-// const driver = neo4j.driver(
-//     process.env.DB_URL ?? '',
-//     neo4j.auth.basic(process.env.DB_USER ?? '', process.env.DB_PASSWORD ?? ''),
-// );
 
 const baseContext = async ({ req }: any) => {
   const token = req.headers['x-token'];
