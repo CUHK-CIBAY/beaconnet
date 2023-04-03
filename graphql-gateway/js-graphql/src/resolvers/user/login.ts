73067dfbd3b3b53dc8937edb3b6432089c0f471f
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-04-04 02:20:46 +0800

    chore: suppress idiot fighting between linter and formatter

--- graphql-gateway/js-graphql/src/resolvers/user/login.ts
+++ graphql-gateway/js-graphql/src/resolvers/user/login.ts
@@ -7,8 +7,10 @@ dotenv.config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+/* eslint-disable */
 const createToken = async ({ id, email, username }: User) =>
   jwt.sign({ id, email, username }, process.env.SERCET, { expiresIn: '1d' });
+/* eslint-enable */
 
 const userLoginResolver = async (_p: any, { input }: any) => {
   const session = driver.session({ database: 'neo4j' });
