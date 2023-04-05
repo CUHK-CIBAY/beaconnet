248af1a1f2d252b47e5535fb35d34f4a3a31db0b
commitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-04-06 03:28:00 +0800

    refactor: separate router to increase modularity

--- beaconnect-react-app/src/App.tsx
+++ beaconnect-react-app/src/App.tsx
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login/login';
-import LoginCheck from './LoginCheck';
-import Main from './pages/Main/main';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router';
 
-function App() {
-  const [isLoggedIn] = useState(false);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <LoginCheck isLoggedIn={isLoggedIn}>
-              <Main />
-            </LoginCheck>
-          }
-        />
-        <Route path="/login" element={<Login loginType="Login" />} />
-        <Route path="/register" element={<Login loginType="Register" />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+);
 
 export default App;
