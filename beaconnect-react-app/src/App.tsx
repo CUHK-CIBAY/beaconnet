2e49be01e922a82cf6b06522d40b1ef3f5aba22f
commitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-04-06 02:36:59 +0800

    refactor: move router inside App

--- beaconnect-react-app/src/App.tsx
+++ beaconnect-react-app/src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Login from './pages/Login/login';
 import LoginCheck from './LoginCheck';
 import Main from './pages/Main/main';
@@ -7,19 +12,21 @@ import Main from './pages/Main/main';
 function App() {
   const [isLoggedIn] = useState(false);
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <LoginCheck isLoggedIn={isLoggedIn}>
-            <Main />
-          </LoginCheck>
-        }
-      />
-      <Route path="/login" element={<Login loginType="Login" />} />
-      <Route path="/register" element={<Login loginType="Register" />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LoginCheck isLoggedIn={isLoggedIn}>
+              <Main />
+            </LoginCheck>
+          }
+        />
+        <Route path="/login" element={<Login loginType="Login" />} />
+        <Route path="/register" element={<Login loginType="Register" />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- beaconnect-react-app/src/index.tsx
+++ beaconnect-react-app/src/index.tsx
@@ -7,8 +7,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
