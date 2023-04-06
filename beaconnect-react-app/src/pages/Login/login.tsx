5e64870632704f01b4952860cd80485f69cb25c6
Anson Cheng <Anson.12666@icloud.com>
2023-04-06 16:55:09 +0800

    chore: remove duplicate html for more react way

--- beaconnect-react-app/src/pages/Login/login.tsx
+++ beaconnect-react-app/src/pages/Login/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import Cover from './images/cover.jpg';
@@ -6,6 +7,53 @@ import './components/login.css';
 const Login = (props: { loginType: string }) => {
   const { loginType } = props;
   const [currentLoginType, setCurrentLoginType] = useState(loginType);
+
+  const handleLoginType = (type: string) => () => {
+    setCurrentLoginType(type === 'Login' ? 'Register' : 'Login');
+    window.history.pushState({}, '', type === 'Login' ? '/register' : '/login');
+  };
+
+  const RegisterField = [
+    {
+      id: 'email',
+      type: 'text',
+      placeholder: 'Enter Here',
+      label: 'Email',
+    },
+    {
+      id: 'username',
+      type: 'text',
+      placeholder: 'Enter Here',
+      label: 'Username',
+    },
+    {
+      id: 'password',
+      type: 'password',
+      placeholder: 'Enter Here',
+      label: 'Password',
+    },
+    {
+      id: 'confirm-password',
+      type: 'password',
+      placeholder: 'Enter Here',
+      label: 'Confirm Password',
+    },
+  ];
+
+  const LoginField = [
+    {
+      id: 'username',
+      type: 'text',
+      placeholder: 'Enter Here',
+      label: 'Username',
+    },
+    {
+      id: 'password',
+      type: 'password',
+      placeholder: 'Enter Here',
+      label: 'Password',
+    },
+  ];
 
   return (
     <div className="Login-Register-Wrapper">
@@ -17,76 +65,24 @@ const Login = (props: { loginType: string }) => {
           <h1>{currentLoginType}</h1>
 
           <form className="Login-Register-Form">
-            {currentLoginType === 'Login' ? (
-              <div className="Login-Register-Form-Group">
+            <div className="Login-Register-Form-Group">
+              {(currentLoginType === 'Login' ? LoginField : RegisterField).map(({ id, type, placeholder, label }) => (
                 <div className="Login-Register-Form-Field">
-                  <input type="text" id="username" className="Login-Register-Form-Input" placeholder="Enter Here" />
-                  <label htmlFor="username" className="Login-Register-Form-Label">
-                    Username
+                  <input type={type} id={id} className="Login-Register-Form-Input" placeholder={placeholder} />
+                  <label htmlFor={id} className="Login-Register-Form-Label">
+                    {label}
                   </label>
                 </div>
-                <div className="Login-Register-Form-Field">
-                  <input type="password" id="password" className="Login-Register-Form-Input" placeholder="Enter Here" />
-                  <label htmlFor="password" className="Login-Register-Form-Label">
-                    Password
-                  </label>
-                </div>
-                <button type="submit" className="Login-Register-Form-Button">
-                  Login
-                </button>
-              </div>
-            ) : (
-              <div className="Login-Register-Form-Group">
-                <div className="Login-Register-Form-Field">
-                  <input type="text" id="email" className="Login-Register-Form-Input" placeholder="Enter Here" />
-                  <label htmlFor="email" className="Login-Register-Form-Label">
-                    Email
-                  </label>
-                </div>
-                <div className="Login-Register-Form-Field">
-                  <input type="text" id="username" className="Login-Register-Form-Input" placeholder="Enter Here" />
-                  <label htmlFor="username" className="Login-Register-Form-Label">
-                    Username
-                  </label>
-                </div>
-                <div className="Login-Register-Form-Field">
-                  <input type="password" id="password" className="Login-Register-Form-Input" placeholder="Enter Here" />
-                  <label htmlFor="password" className="Login-Register-Form-Label">
-                    Password
-                  </label>
-                </div>
-                <div className="Login-Register-Form-Field">
-                  <input
-                    type="password"
-                    id="confirm-password"
-                    className="Login-Register-Form-Input"
-                    placeholder="Enter Here"
-                  />
-                  <label htmlFor="confirm-password" className="Login-Register-Form-Label">
-                    Confirm Password
-                  </label>
-                </div>
-                <button type="submit" className="Login-Register-Form-Button">
-                  Register
-                </button>
-              </div>
-            )}
+              ))}
+              <button type="submit" className="Login-Register-Form-Button">
+                {currentLoginType}
+              </button>
+            </div>
           </form>
         </div>
         <div className="Login-Register-Form-Footer">
           <p>
-            <button
-              type="button"
-              onClick={() => {
-                if (currentLoginType === 'Login') {
-                  setCurrentLoginType('Register');
-                  window.history.pushState({}, '', '/register');
-                } else {
-                  setCurrentLoginType('Login');
-                  window.history.pushState({}, '', '/login');
-                }
-              }}
-            >
+            <button type="button" onClick={handleLoginType(currentLoginType)}>
               {currentLoginType === 'Login' ? 'Create Account' : 'Already have an account?'}
             </button>
           </p>
