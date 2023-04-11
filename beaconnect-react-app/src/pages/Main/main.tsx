956ef5f0d0da5f38cc6060e60900a5918578657f
Anson Cheng <Anson.12666@icloud.com>
2023-04-12 07:19:01 +0800

    feat: create profile page

--- beaconnect-react-app/src/pages/Main/main.tsx
+++ beaconnect-react-app/src/pages/Main/main.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Search from './pages/Search/search';
+import Profile from './pages/Profile/profile';
 import NavBar from './components/NavBar/navBar';
 import './components/main.css';
 import './pages/Home/components/home.css';
@@ -12,6 +13,7 @@ const Main = () => (
     <Suspense fallback={<p>loading</p>}>
       <Routes>
         <Route path="/search" element={<Search />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Homepage />} />
       </Routes>
     </Suspense>
