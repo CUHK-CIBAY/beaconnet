12f86ec9f0deae2da7cc9630ec17fcc2ed6709d8
Anson Cheng <Anson.12666@icloud.com>
2023-04-12 07:29:03 +0800

    feat: add profile page

---
+++ beaconnect-react-app/src/pages/Main/pages/Profile/profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="page-content">
+    <div className="page-center-content">aaa</div>
+    <div className="page-right-content">aaa</div>
+  </div>
+);
+
+export default Profile;
