e26b0b3b3f641eb400c8039cd5361da04ade6e3f
Anson Cheng <Anson.12666@icloud.com>
2023-04-12 07:28:44 +0800

    fix: make bit box comment stay active when typed

--- beaconnect-react-app/src/pages/Main/components/Bits/bits.tsx
+++ beaconnect-react-app/src/pages/Main/components/Bits/bits.tsx
@@ -54,68 +54,85 @@ export const WriteBitBox = () => {
   );
 };
 
-export const BitBox = ({ haveCaption, isRepost }: { haveCaption?: boolean; isRepost?: boolean }) => (
-  <div className="bit-box bit-box-container">
-    {isRepost && (
-      <div className="bit-box-repost-statement">
-        <BiRepost className="bit-box-repost-statement-icon" />
-        <p>Rebit by Username</p>
-      </div>
-    )}
-    <div className="bit-box-content-header">
-      <img className="bit-box-icon" src={userIcon} alt="profile" />
-      <div className="bit-box-content-header-name">John Doe</div>
-      <div className="bit-box-content-header-userID">@johndoe</div>
-      <div className="bit-box-content-header-time">2 hours ago</div>
-    </div>
-    <div className="bit-box-content">
-      <div className="bit-box-content-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
-        aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl aliquet
-        nisl, eget aliquet nisl nisl eget nisl.
-      </div>
-    </div>
-    {haveCaption && (
-      <div className="bit-box-rebit-with-caption">
-        <div className="bit-box-content-header">
-          <img className="bit-box-icon" src={userIcon} alt="profile" />
-          <div className="bit-box-content-header-name">John Doe</div>
-          <div className="bit-box-content-header-userID">@johndoe</div>
-          <div className="bit-box-content-header-time">2 hours ago</div>
+export const BitBox = ({ haveCaption, isRepost }: { haveCaption?: boolean; isRepost?: boolean }) => {
+  const addActiveStatus = (e: React.FocusEvent) => {
+    const { currentTarget } = e;
+    const parent = currentTarget.parentElement?.parentElement?.parentElement as HTMLDivElement;
+    parent.classList.add('active');
+  };
+
+  const removeActiveStatus = (e: React.FocusEvent) => {
+    const { currentTarget, target } = e;
+    const textBox = target as HTMLTextAreaElement;
+    const currentInput = textBox.value;
+    if (currentInput.length === 0) {
+      const parent = currentTarget.parentElement?.parentElement?.parentElement as HTMLDivElement;
+      parent.classList.remove('active');
+    }
+  };
+
+  return (
+    <div className="bit-box bit-box-container">
+      {isRepost && (
+        <div className="bit-box-repost-statement">
+          <BiRepost className="bit-box-repost-statement-icon" />
+          <p>Rebit by Username</p>
         </div>
-        <div className="bit-box-content">
-          <div className="bit-box-content-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl
-            nisl aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
-            aliquet nisl, eget aliquet nisl nisl eget nisl.
+      )}
+      <div className="bit-box-content-header">
+        <img className="bit-box-icon" src={userIcon} alt="profile" />
+        <div className="bit-box-content-header-name">John Doe</div>
+        <div className="bit-box-content-header-userID">@johndoe</div>
+        <div className="bit-box-content-header-time">2 hours ago</div>
+      </div>
+      <div className="bit-box-content">
+        <div className="bit-box-content-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
+          aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl aliquet
+          nisl, eget aliquet nisl nisl eget nisl.
+        </div>
+      </div>
+      {haveCaption && (
+        <div className="bit-box-rebit-with-caption">
+          <div className="bit-box-content-header">
+            <img className="bit-box-icon" src={userIcon} alt="profile" />
+            <div className="bit-box-content-header-name">John Doe</div>
+            <div className="bit-box-content-header-userID">@johndoe</div>
+            <div className="bit-box-content-header-time">2 hours ago</div>
+          </div>
+          <div className="bit-box-content">
+            <div className="bit-box-content-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl
+              nisl aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
+              aliquet nisl, eget aliquet nisl nisl eget nisl.
+            </div>
           </div>
         </div>
+      )}
+      <div className="bit-box-content-footer">
+        <div className="bit-box-content-footer-likes bit-box-content-footer-icons">
+          <AiOutlineHeart />
+          <p>10 likes</p>
+        </div>
+        <div className="bit-box-content-footer-comments bit-box-content-footer-icons">
+          <BiComment />
+          <p>5 comments</p>
+        </div>
+        <div className="bit-box-content-footer-repost bit-box-content-footer-icons">
+          <BiRepost />
+        </div>
       </div>
-    )}
-    <div className="bit-box-content-footer">
-      <div className="bit-box-content-footer-likes bit-box-content-footer-icons">
-        <AiOutlineHeart />
-        <p>10 likes</p>
-      </div>
-      <div className="bit-box-content-footer-comments bit-box-content-footer-icons">
-        <BiComment />
-        <p>5 comments</p>
-      </div>
-      <div className="bit-box-content-footer-repost bit-box-content-footer-icons">
-        <BiRepost />
+      <div className="bit-box-content-footer-comment">
+        <div className="bit-box-content-footer-comment-input">
+          <input type="text" placeholder="Write a comment..." onFocus={addActiveStatus} onBlur={removeActiveStatus} />
+        </div>
+        <div className="bit-box-content-footer-comment-submit">
+          <TbSend />
+        </div>
       </div>
     </div>
-    <div className="bit-box-content-footer-comment">
-      <div className="bit-box-content-footer-comment-input">
-        <input type="text" placeholder="Write a comment..." />
-      </div>
-      <div className="bit-box-content-footer-comment-submit">
-        <TbSend />
-      </div>
-    </div>
-  </div>
-);
-
+  );
+};
 BitBox.defaultProps = {
   haveCaption: false,
   isRepost: false,
