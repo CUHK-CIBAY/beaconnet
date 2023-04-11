df54260fd5c621e09f3fcab985ba117125e2774f
Chiubiu <wtcrystalc@gmail.com>
2023-04-11 22:02:16 +0800

    feat: finished main page layout

---
+++ beaconnect-react-app/src/pages/Main/components/Bits/bits.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BiComment, BiRepost } from 'react-icons/bi';
+import { BsSoundwave, BsImage } from 'react-icons/bs';
+import { FiVideo } from 'react-icons/fi';
+import { TbSend } from 'react-icons/tb';
+import userIcon from '../../pages/Home/components/icon.png';
+
+export const WriteBitBox = () => (
+  <div className="write-bit-box bit-box-container">
+    <img className="bit-box-icon" src={userIcon} alt="profile" />
+    <div className="write-bit-box-content">
+      <textarea className="write-bit-box-content-text" placeholder="Write something..." />
+      <div className="write-bit-box-options">
+        <div className="write-bit-box-options-attachment">
+          <BsImage className="write-bit-box-options-icon" />
+          <FiVideo className="write-bit-box-options-icon" />
+          <BsSoundwave className="write-bit-box-options-icon" />
+        </div>
+        <div className="write-bit-box-options-submit">
+          <TbSend className="write-bit-box-options-submit-icon" />
+          <input type="submit" value="Send Bit" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export const BitBox = ({ haveCaption, isRepost }: { haveCaption?: boolean; isRepost?: boolean }) => (
+  <div className="bit-box bit-box-container">
+    {isRepost && (
+      <div className="bit-box-repost-statement">
+        <BiRepost className="bit-box-repost-statement-icon" />
+        <p>Rebit by Username</p>
+      </div>
+    )}
+    <div className="bit-box-content-header">
+      <img className="bit-box-icon" src={userIcon} alt="profile" />
+      <div className="bit-box-content-header-name">John Doe</div>
+      <div className="bit-box-content-header-userID">@johndoe</div>
+      <div className="bit-box-content-header-time">2 hours ago</div>
+    </div>
+    <div className="bit-box-content">
+      <div className="bit-box-content-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
+        aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl aliquet
+        nisl, eget aliquet nisl nisl eget nisl.
+      </div>
+    </div>
+    {haveCaption && (
+      <div className="bit-box-rebit-with-caption">
+        <div className="bit-box-content-header">
+          <img className="bit-box-icon" src={userIcon} alt="profile" />
+          <div className="bit-box-content-header-name">John Doe</div>
+          <div className="bit-box-content-header-userID">@johndoe</div>
+          <div className="bit-box-content-header-time">2 hours ago</div>
+        </div>
+        <div className="bit-box-content">
+          <div className="bit-box-content-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl
+            nisl aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
+            aliquet nisl, eget aliquet nisl nisl eget nisl.
+          </div>
+        </div>
+      </div>
+    )}
+    <div className="bit-box-content-footer">
+      <div className="bit-box-content-footer-likes bit-box-content-footer-icons">
+        <AiOutlineHeart />
+        <p>10 likes</p>
+      </div>
+      <div className="bit-box-content-footer-comments bit-box-content-footer-icons">
+        <BiComment />
+        <p>5 comments</p>
+      </div>
+      <div className="bit-box-content-footer-repost bit-box-content-footer-icons">
+        <BiRepost />
+      </div>
+    </div>
+    <div className="bit-box-content-footer-comment">
+      <div className="bit-box-content-footer-comment-input">
+        <input type="text" placeholder="Write a comment..." />
+      </div>
+      <div className="bit-box-content-footer-comment-submit">
+        <TbSend />
+      </div>
+    </div>
+  </div>
+);
+
+BitBox.defaultProps = {
+  haveCaption: false,
+  isRepost: false,
+};
--- beaconnect-react-app/src/pages/Main/main.tsx
+++ beaconnect-react-app/src/pages/Main/main.tsx
@@ -1,70 +1,10 @@
 import React from 'react';
-import { AiOutlineHeart } from 'react-icons/ai';
-import { BiComment } from 'react-icons/bi';
-import { BsSoundwave, BsImage } from 'react-icons/bs';
-import { FiVideo } from 'react-icons/fi';
-import { TbSend } from 'react-icons/tb';
+import { BiSearchAlt } from 'react-icons/bi';
+import { WriteBitBox, BitBox } from './components/Bits/bits';
 import NavBar from './components/NavBar/navBar';
+import seasonalEvent from './pages/Home/components/seasonalpic.jpg';
 import './components/main.css';
 import './pages/Home/components/home.css';
-
-import userIcon from './pages/Home/components/icon.png';
-
-const WriteBitBox = () => (
-  <div className="write-bit-box bit-box-container">
-    <img className="bit-box-icon" src={userIcon} alt="profile" />
-    <div className="write-bit-box-content">
-      <textarea className="write-bit-box-content-text" placeholder="Write something..." />
-      <div className="write-bit-box-options">
-        <div className="write-bit-box-options-attachment">
-          <BsImage className="write-bit-box-options-icon" />
-          <FiVideo className="write-bit-box-options-icon" />
-          <BsSoundwave className="write-bit-box-options-icon" />
-        </div>
-        <div className="write-bit-box-options-submit">
-          <TbSend className="write-bit-box-options-submit-icon" />
-          <input type="submit" value="Send Bit" />
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-const BitBox = () => (
-  <div className="bit-box bit-box-container">
-    <div className="bit-box-content-header">
-      <img className="bit-box-icon" src={userIcon} alt="profile" />
-      <div className="bit-box-content-header-name">John Doe</div>
-      <div className="bit-box-content-header-userID">@johndoe</div>
-      <div className="bit-box-content-header-time">2 hours ago</div>
-    </div>
-    <div className="bit-box-content">
-      <div className="bit-box-content-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget tincidunt lacinia, nisl nisl
-        aliquet nisl, eget aliquet nisl nisl eget nisl. Donec auctor, nisl eget tincidunt lacinia, nisl nisl aliquet
-        nisl, eget aliquet nisl nisl eget nisl.
-      </div>
-    </div>
-    <div className="bit-box-content-footer">
-      <div className="bit-box-content-footer-likes bit-box-content-footer-icons">
-        <AiOutlineHeart />
-        <p>10 likes</p>
-      </div>
-      <div className="bit-box-content-footer-comments bit-box-content-footer-icons">
-        <BiComment />
-        <p>5 comments</p>
-      </div>
-    </div>
-    <div className="bit-box-content-footer-comment">
-      <div className="bit-box-content-footer-comment-input">
-        <input type="text" placeholder="Write a comment..." />
-      </div>
-      <div className="bit-box-content-footer-comment-submit">
-        <TbSend />
-      </div>
-    </div>
-  </div>
-);
 
 const Main = () => (
   <div className="homePage">
@@ -72,10 +12,30 @@ const Main = () => (
     <div className="page-content">
       <div className="page-center-content">
         <WriteBitBox />
-        <BitBox />
+        <BitBox isRepost haveCaption />
+        <BitBox isRepost />
+        <BitBox isRepost />
+        <BitBox isRepost />
+        <BitBox isRepost />
+        <BitBox isRepost />
       </div>
-      <div className="homePage-right-content">
-        <p>bbb</p>
+      <div className="page-right-content">
+        <img className="profile-background-picture" src={seasonalEvent} alt="profile" />
+        <div className="trend-searching-container">
+          <div className="trend-search-bar">
+            <div className="trend-search-bar-icon">
+              <BiSearchAlt />
+            </div>
+            <input type="text" placeholder="search" />
+          </div>
+          <div className="trend-suggestion-section">
+            <h2>Trends for you</h2>
+            <div className="trend-suggestion-list">
+              <p className="trend-suggestion-item">GitHub</p>
+              <p className="trend-suggestion-item-size">2.5M Bit</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
