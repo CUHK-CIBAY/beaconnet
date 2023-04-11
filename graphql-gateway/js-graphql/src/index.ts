9dcab80ce9528efced8c90e3cd7b302915b43221
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-04-11 12:42:28 +0800

    feat: migrate from apollo to yoga and export handler. Temp remove context

--- graphql-gateway/js-graphql/src/index.ts
+++ graphql-gateway/js-graphql/src/index.ts
@@ -1,20 +1,36 @@
-import { ApolloServer } from 'apollo-server';
+import { createSchema, createYoga } from 'graphql-yoga';
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
 import typeDefs from './schemas';
 import resolvers from './resolvers';
 
 import baseContext from './context/baseContext';
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs: await typeDefs,
-    resolvers,
-    context: baseContext,
+export const handler = async (event: APIGatewayEvent, lambdaContext: Context): Promise<APIGatewayProxyResult> => {
+  const yoga = createYoga<{
+    event: APIGatewayEvent;
+    lambdaContext: Context;
+  }>({
+    schema: createSchema({
+      typeDefs: await typeDefs,
+      resolvers,
+    }),
   });
+  const response = await yoga.fetch(
+    event.path + '?' + new URLSearchParams((event.queryStringParameters as Record<string, string>) || {}).toString(),
+    {
+      method: event.httpMethod,
+      headers: event.headers as HeadersInit,
+      body: event.body ? Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8') : undefined,
+    },
+    { event, lambdaContext },
+  );
+  const responseHeaders = Object.fromEntries(response.headers.entries());
 
-  const { url } = await server.listen();
-
-  console.log(`? Server ready at ${url}`);
+  return {
+    statusCode: response.status,
+    headers: responseHeaders,
+    body: await response.text(),
+    isBase64Encoded: false,
+  };
 };
-
-startServer();
