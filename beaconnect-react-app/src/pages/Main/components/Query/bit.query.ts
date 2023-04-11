d5c1f8377e6336e7ba29275e0a2aa34b2d4b5948
Anson Cheng <Anson.12666@icloud.com>
2023-04-12 01:15:10 +0800

    feat: create function for get bits for homepage

--- beaconnect-react-app/src/pages/Main/components/Query/bit.query.ts
+++ beaconnect-react-app/src/pages/Main/components/Query/bit.query.ts
@@ -1,19 +1,51 @@
 import { gql } from '@apollo/client';
 
 export type postBitMutationVariables = {
-    content: string;
+  content: string;
 };
 
 export type postBitMutationResult = {
-    postBit: {
-        id: string;
-    };
+  postBit: {
+    id: string;
+  };
 };
 
 export const postBitQuery = gql`
-    mutation PostBit($content: String!) {
-        postBit(content: $content) {
+  mutation PostBit($content: String!) {
+    postBit(content: $content) {
+      id
+    }
+  }
+`;
+
+
+export type getBitsQueryVariables = {
+    limit: number;
+    offset: number;
+};
+
+export type getBitsQueryResult = {
+    bits: {
+        id: string;
+        content: string;
+        createdAt: string;
+        user: {
+            id: string;
+            name: string;
+        };
+    }[];
+};
+
+export const getBitsQuery = gql`
+    query GetBits($limit: Int!, $offset: Int!) {
+        bits(limit: $limit, offset: $offset) {
             id
+            content
+            createdAt
+            user {
+                id
+                name
+            }
         }
     }
 `;
