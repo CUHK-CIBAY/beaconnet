4452e873287a2b4e36972935f6de97de093d6284
Chiubiu <121796751+Chiubiu@users.noreply.github.com>
2023-04-12 21:35:55 +0800

    Feat: search page (#62)
    * chore: update new server endpoint
    
    * feat: create route
    
    * fix: make bit box comment stay active when typed
    
    * feat: create profile page
    
    * fix: revert to last version
    
    * fix: Update main.tsx
    
    * feat: search_page_without_css
    
    * feat: search_page_with_css
    
    * feat: search_page_with_css_v2
    
    * chore: separate lambda version and local version server
    
    * style: formatter
    
    ---------
    
    Co-authored-by: Anson Cheng <Anson.12666@icloud.com>
    Co-authored-by: isaackhlam <97761927+isaackhlam@users.noreply.github.com>

--- graphql-gateway/js-graphql/src/index.ts
+++ graphql-gateway/js-graphql/src/index.ts
@@ -1,58 +1,23 @@
 import { createSchema, createYoga } from 'graphql-yoga';
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-//import { createServer } from 'http';
+import { createServer } from 'http';
 
 import typeDefs from './schemas';
 import resolvers from './resolvers';
 
 import baseContext from './context/baseContext';
 
-// Uncomment the following line to run locally
-//const main = async () => {
-  //const yoga = createYoga({
-    //schema: createSchema(
-      //{
-        //typeDefs: await typeDefs,
-        //resolvers
-      //}
-    //),
-    //context: baseContext
-  //});
-  //const server = createServer(yoga);
-  //server.listen(4000, () => {
-    //console.info('Server is running on http://localhost:4000/graphql')
-  //})
-//}
-
-//main()
-// Uncomment the above line to run locally
-
-export const handler = async (event: APIGatewayEvent, lambdaContext: Context): Promise<APIGatewayProxyResult> => {
-  const yoga = createYoga<{
-    event: APIGatewayEvent;
-    lambdaContext: Context;
-  }>({
+const main = async () => {
+  const yoga = createYoga({
     schema: createSchema({
       typeDefs: await typeDefs,
       resolvers,
     }),
-    context: baseContext
+    context: baseContext,
   });
-  const response = await yoga.fetch(
-    event.path + '?' + new URLSearchParams((event.queryStringParameters as Record<string, string>) || {}).toString(),
-    {
-      method: event.httpMethod,
-      headers: event.headers as HeadersInit,
-      body: event.body ? Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8') : undefined,
-    },
-    { event, lambdaContext },
-  );
-  const responseHeaders = Object.fromEntries(response.headers.entries());
-
-  return {
-    statusCode: response.status,
-    headers: responseHeaders,
-    body: await response.text(),
-    isBase64Encoded: false,
-  };
+  const server = createServer(yoga);
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql');
+  });
 };
+
+main();
