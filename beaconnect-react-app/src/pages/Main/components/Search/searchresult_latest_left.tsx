163ea00aa0bfe01d820011aaf3d193c3885ce667
Anson Cheng <Anson.12666@icloud.com>
2023-04-13 06:18:48 +0800

    other: mock data

--- beaconnect-react-app/src/pages/Main/components/Search/searchresult_latest_left.tsx
+++ beaconnect-react-app/src/pages/Main/components/Search/searchresult_latest_left.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { BitBox } from '../Bits/bits';
 
+const fakeData = {
+  id: '1',
+  content: '123',
+  createAt: '2023-04-11T16:02:48.611Z',
+  totalLike: 0,
+  author: null,
+  __typename: 'Bit',
+};
+
+/* eslint-disable */
 const SearchResultLatest = () => (
   <div className="search-result-lastest-section">
     <div className="search-result-lastest-header">
@@ -12,11 +22,11 @@ const SearchResultLatest = () => (
       </div>
     </div>
     <div className="search-result-lastest">
-      <BitBox isRepost haveCaption />
-      <BitBox isRepost />
-      <BitBox havePhoto />
-      <BitBox />
-      <BitBox />
+      <BitBox {...fakeData} />
+      <BitBox {...fakeData} />
+      <BitBox {...fakeData} />
+      <BitBox {...fakeData} />
+      <BitBox {...fakeData} />
     </div>
   </div>
 );
