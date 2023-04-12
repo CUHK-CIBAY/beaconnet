570c6e7450f53fec00d8d23151412fd271e3bf03
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-04-12 21:30:27 +0800

    chore: separate lambda version and local version server

--- graphql-gateway/js-graphql/src/index.ts
+++ graphql-gateway/js-graphql/src/index.ts
@@ -1,5 +1,4 @@
 import { createSchema, createYoga } from 'graphql-yoga';
-// import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { createServer } from 'http';
 
 import typeDefs from './schemas';
@@ -7,7 +6,6 @@ import resolvers from './resolvers';
 
 import baseContext from './context/baseContext';
 
-// Uncomment the following line to run locally
 const main = async () => {
   const yoga = createYoga({
     schema: createSchema(
@@ -24,35 +22,4 @@ const main = async () => {
   })
 }
 
-main()
-// Uncomment the above line to run locally
-
-// export const handler = async (event: APIGatewayEvent, lambdaContext: Context): Promise<APIGatewayProxyResult> => {
-//   const yoga = createYoga<{
-//     event: APIGatewayEvent;
-//     lambdaContext: Context;
-//   }>({
-//     schema: createSchema({
-//       typeDefs: await typeDefs,
-//       resolvers,
-//     }),
-//     context: baseContext
-//   });
-//   const response = await yoga.fetch(
-//     event.path + '?' + new URLSearchParams((event.queryStringParameters as Record<string, string>) || {}).toString(),
-//     {
-//       method: event.httpMethod,
-//       headers: event.headers as HeadersInit,
-//       body: event.body ? Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8') : undefined,
-//     },
-//     { event, lambdaContext },
-//   );
-//   const responseHeaders = Object.fromEntries(response.headers.entries());
-
-//   return {
-//     statusCode: response.status,
-//     headers: responseHeaders,
-//     body: await response.text(),
-//     isBase64Encoded: false,
-//   };
-// };
+main();
