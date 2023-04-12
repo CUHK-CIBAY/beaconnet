4bf7ecfa3a2e372f23d8460864e8ed2e9cd3a630
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-04-13 02:30:03 +0800

    feat: update logined user info

--- graphql-gateway/js-graphql/src/resolvers/user/user-info-impl.ts
+++ graphql-gateway/js-graphql/src/resolvers/user/user-info-impl.ts
@@ -4,17 +4,33 @@ import driver from '../../util/neo4j-driver';
 
 dotenv.config();
 
-export const userInfoResolver = ({ info }: User) => ({ gender: info?.gender });
-
-export const updateUserInfo = async (_p: any, { input }: MutationUpdateInfoArgs): Promise<User | null> => {
+export const userInfoResolver = async ({ id }: User) => {
   const session = driver.session({ database: 'neo4j' });
   try {
     const query = `
-            MERGE (u:User {id: '1'})-[:HAS]->(i:UserInfo)
-            SET i = $input
+            MATCH (:User {id: $id})-[:HAS]->(i:UserInfo)
+            RETURN i
+        `;
+    const result = await session.run(query, { id });
+    return result.records[0]?.get('i').properties;
+  } catch (error) {
+    console.error(error);
+    return null;
+  } finally {
+    await session.close();
+  }
+};
+
+export const updateUserInfo = async (_p: any, { input }: MutationUpdateInfoArgs, { me }: any ): Promise<User | null> => {
+  const session = driver.session({ database: 'neo4j' });
+  try {
+    const query = `
+            MATCH (u:User {id: $id})
+            MERGE (u)-[:HAS]->(i:UserInfo)
+            SET i += $input
             RETURN u
         `;
-    const result = await session.run(query, { input });
+    const result = await session.run(query, { input, id: me.id });
     return result.records[0].get('u').properties;
   } catch (error) {
     console.error(error);
