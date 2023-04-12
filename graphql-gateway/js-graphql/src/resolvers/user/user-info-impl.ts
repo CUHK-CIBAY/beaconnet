27ffb03c635b22c2f86cb1fcbbde7cd04ac23926
BillyLam1023 <billylam741987@gmail.com>
2023-04-13 01:29:08 +0800

    feat: add user-info

--- graphql-gateway/js-graphql/src/resolvers/user/user-info-impl.ts
+++ graphql-gateway/js-graphql/src/resolvers/user/user-info-impl.ts
@@ -4,16 +4,27 @@ import driver from '../../util/neo4j-driver';
 
 dotenv.config();
 
-export const userInfoResolver = ({ info }: User) => ({ gender: info?.gender});
+export const userInfoResolver = ({ info }: any) => ({
+  gender: info?.gender,
+  username: info?.username,
+  phoneNumber: info?.phoneNumber,
+  email: info?.email,
+  languages: info?.languages,
+  birthDate: info?.birthDate,
+  age: info?.age,
+});
 
-export const updateUserInfo = async (_p: any, { input }: MutationUpdateInfoArgs): Promise<User | null> => {
+export const updateUserInfo = async (
+  _p: any,
+  { input }: MutationUpdateInfoArgs
+): Promise<User | null> => {
   const session = driver.session({ database: 'neo4j' });
   try {
     const query = `
-            MERGE (u:User {id: '1'})-[:HAS]->(i:UserInfo)
-            SET i = $input
-            RETURN u
-        `;
+      MERGE (u:User {id: '1'})-[:HAS]->(i:UserInfo)
+      SET i += $input
+      RETURN u
+    `;
     const result = await session.run(query, { input });
     return result.records[0].get('u').properties;
   } catch (error) {
