a66f1a506b74bf77b2b74d6141013cfa150d5eee
Chiubiu <121796751+Chiubiu@users.noreply.github.com>
2023-04-14 07:16:26 +0800

    feat: admin panel function (#103)
    * feat: show user list on admin panel
    
    * feat: admin page delete user done
    
    * feat: delete user in admin page

---
+++ beaconnect-react-app/src/pages/Main/components/Query/user.query.ts
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export type showUsersListQueryResult = {
+  users: [
+    {
+      username: string;
+      id: string;
+      info: {
+        nickname: string;
+      };
+    },
+  ];
+};
+
+export const showUsersListQuery = gql`
+  query {
+    users {
+      username
+      id
+      info {
+        nickname
+      }
+    }
+  }
+`;
+
+export type deleteUserMutationVariables = {
+  id: string;
+};
+
+export type deleteUserMutationResult = {
+  boolean: false;
+};
+
+export const deleteUserQuery = gql`
+  mutation MyMutation($id: ID!) {
+    deleteUser(id: $id)
+  }
+`;
