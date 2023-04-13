d41827e631b9f67a4373f47bc221b32485651165
Yan2637291 <121821787+Yan2637291@users.noreply.github.com>
2023-04-14 01:48:46 +0800

    feat: Frontend setting page (#91)
    * feat: frontend setting page
    
    * feat: frontend setting page

---
+++ beaconnect-react-app/src/pages/Main/pages/Setting/components/setting_Notification.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { RiArrowRightSLine } from 'react-icons/ri';
+
+const SettingNotification = () => (
+  <div className="setting-notification-container">
+    <div className="setting-notification-header">Notification</div>
+    <div className="notification-filter">
+      <div>
+        <p className="notification-filter-header">Filter</p>
+        <div className="notification-filter-content">
+          Choose the notification you&apos;d like to see and those you don&apos;t
+        </div>
+      </div>
+      <div className="notification-filter-arrow">
+        <RiArrowRightSLine />
+      </div>
+    </div>
+    <div className="notification-perferences">
+      <div>
+        <p className="notification-perferences-header">Perferences</p>
+        <div className="notification-perferences-content">Select your perference by notification type</div>
+      </div>
+      <div className="notification-perferences-arrow">
+        <RiArrowRightSLine />
+      </div>
+    </div>
+  </div>
+);
+
+export default SettingNotification;
