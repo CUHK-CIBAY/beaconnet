e2fa057b2f34626dce1649aff1515a0077d97e85
BillyLam1023 <billylam741987@gmail.com>
2023-04-13 17:35:29 +0800

    feat: Liked-Bit

---
+++ beaconnect-react-app/src/pages/Main/components/Bits/LikedBit.tsx
@@ -0,0 +1,30 @@
+import { useMutation } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const LIKE_POST_MUTATION = gql`
+  mutation LikedBit($postId: ID!) {
+    likedBit(postId: $postId) {
+      id
+      likes {
+        id
+        user {
+          id
+          username
+        }
+      }
+    }
+  }
+`;
+
+const likedBitHandler = async (postId) => {
+  try {
+    const { data } = await likedBit({
+      variables: { postId },
+    });
+    return data.likedBit.likes.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const [likedBit] = useMutation(LIKE_POST_MUTATION);
