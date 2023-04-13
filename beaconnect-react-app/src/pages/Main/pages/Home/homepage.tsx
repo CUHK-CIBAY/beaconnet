971ee8fd539ad575e057b54876816ebafc176cb1
Yan2637291 <121821787+Yan2637291@users.noreply.github.com>
2023-04-14 06:57:59 +0800

    feat: home page update (#100)
    * feat: home page right UI fix
    
    * feat: home page right container UI fix

--- beaconnect-react-app/src/pages/Main/pages/Home/homepage.tsx
+++ beaconnect-react-app/src/pages/Main/pages/Home/homepage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { BiSearchAlt } from 'react-icons/bi';
+// import { BiSearchAlt } from 'react-icons/bi';
 import { WriteBitBox, BitBox } from '../../components/Bits/bits';
 import { showBitsQuery, showBitsQueryVariables, showBitsQueryResult } from '../../components/Query/bit.query';
 import seasonalEvent from './components/seasonalpic.jpg';
@@ -31,7 +31,22 @@ const Home = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
       <div className="page-right-content">
         <img className="profile-background-picture" src={seasonalEvent} alt="profile" />
         <div className="trend-searching-container">
-          <div className="trend-search-bar">
+          <div className="welcome-speech-container">
+            <div className="welcome-speech-container-header">Welcome to Beaconnnect!!!</div>
+            <div>You can sending Bits to your friend with picture, video and voice.</div>
+            <div>There are some function at the left hand side, such as</div>
+            <div>
+              <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>Search</li>
+                <li>Help</li>
+                <li>Setting</li>
+              </ul>
+            </div>
+            <div>If you help any problem, you can find the help center at the left hand side.</div>
+          </div>
+          {/* <div className="trend-search-bar">
             <div className="trend-search-bar-icon">
               <BiSearchAlt />
             </div>
@@ -43,7 +58,7 @@ const Home = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
               <p className="trend-suggestion-item">GitHub</p>
               <p className="trend-suggestion-item-size">2.5M Bit</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
