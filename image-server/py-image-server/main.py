f487fe6a01a1441c986480aac7fcb444051beccf
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-04-13 19:48:50 +0800

    feat: image server can upload and return file now

--- image-server/py-image-server/main.py
+++ image-server/py-image-server/main.py
@@ -3,34 +3,26 @@ import traceback
 import io
 import os
 import json
+import uuid
+import base64
 
 def upload_to_S3(file, s3_key):
     s3 = boto3.client('s3')
     try:
         fileobj = io.BytesIO(file)
         bucket = boto3.resource("s3").Bucket(os.environ['BUCKET_NAME'])
-        print("before upload fileobj")
-        bucket.upload_fileobj(fileobj, s3_key)
-        print("before get url")
-        url = s3.generate_presigned_url(
-                ClientMethod = 'get_object',
-                Params = {
-                    'Bucket': os.environ['BUCKET_NAME'],
-                    'Key': s3_key
-                },
-                ExpiresIn = 24 * 3600
-            )
-
-        print("Upload Successfull {}".format(url))
+        bucket.upload_fileobj(fileobj, s3_key, {"ACL": "public-read"})
         return {
-            "statusCode": 200,
-            "body": {
-                "Test-string": "hello",
-                "url": url,    
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST,GET"
             },
+            "statusCode": 200,
+            "body": json.dumps({
+                "key": s3_key,
+            }),
         }
     except Exception as ex:
-        print(ex)
         print(traceback.format_exc())
         return {
             "statusCode": 400,
@@ -42,12 +34,18 @@ def get_object_from_S3(s3_key):
     try:
         file_obj = s3.get_object(Bucket = os.environ['BUCKET_NAME'], Key = s3_key)
         file = file_obj["Body"].read()
+        print(type(file))
         return {
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST,GET",
+                "Content-type": "image"
+            },
             "statusCode": 200,
-            "body": json.dumps(json.loads(file.decode('utf8')))
+            "body": base64.b64encode(file),
+            "isBase64Encoded": True,
         }
     except Exception as ex:
-        print(ex)
         print(traceback.format_exc())
         return {
             "statusCode": 400,
@@ -56,8 +54,15 @@ def get_object_from_S3(s3_key):
 
 def handler(event, context):
     print(event)
-    print(context)
-    resp = upload_to_S3(json.dumps(event).encode('utf-8'), "testing")
-    resp = get_object_from_S3("testing")
+    headers = event["headers"]
+    method = event["httpMethod"]
+    if method == "POST":
+        file = event["body"]
+        print("type of body: {}".format(type(file)))
+        resp = upload_to_S3(base64.b64decode(file), str(uuid.uuid4()))
+    else:
+        query = event["queryStringParameters"]
+        s3_key = query["image"]
+        resp = get_object_from_S3(s3_key)
     print(resp)
     return resp;
