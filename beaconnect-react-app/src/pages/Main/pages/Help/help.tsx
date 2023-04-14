dc418f973eb1704211c39714174213414cd55a06
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-04-14 14:02:15 +0800

    feat: help page (#118)
    Co-authored-by: Anson Cheng <Anson.12666@icloud.com>

--- beaconnect-react-app/src/pages/Main/pages/Help/help.tsx
+++ beaconnect-react-app/src/pages/Main/pages/Help/help.tsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import './help.css';
 
-const Help = () => (
-  <div className="page-content-help">
-    <div className="help-main-content-container">
-      <div className="help-main-header">Help Center</div>
-      <div className="help-main-description">
-        <p>In the help center, you can report the problem you are dealing with.</p>
-        <p>After reporting it, we will solve the problem within few day.</p>
-      </div>
-      <div className="help-main-report-header">Report Problem</div>
-      <div className="help-main-report-content">
-        <div className="help-main-report-content-header">
-          <div className="help-main-report-content-title">
-            <input type="text" className="help-main-report-content-title-input" placeholder="Title" />
-          </div>
-          <div className="help-main-report-content-button">
-            <button type="submit" className="help-main-report-content-submit">
-              Report
-            </button>
-          </div>
+const Help = () => {
+  const clearAllField = () => {
+    const title = document.querySelector('.help-main-report-content-title-input') as HTMLInputElement;
+    const description = document.querySelector('.help-main-report-content-description-input') as HTMLInputElement;
+    title.value = '';
+    description.value = '';
+  };
+
+  return (
+    <div className="page-content-help">
+      <div className="help-main-content-container">
+        <div className="help-main-header">Help Center</div>
+        <div className="help-main-description">
+          <p>In the help center, you can report the problem you are dealing with.</p>
+          <p>After reporting it, we will solve the problem within few day.</p>
         </div>
-        <div className="help-main-report-content-description">
-          <textarea className="help-main-report-content-description-input" placeholder="Descrition" />
+        <div className="help-main-report-header">Report Problem</div>
+        <div className="help-main-report-content">
+          <div className="help-main-report-content-header">
+            <div className="help-main-report-content-title">
+              <input type="text" className="help-main-report-content-title-input" placeholder="Title" />
+            </div>
+            <div
+              className="help-main-report-content-button"
+              onClick={clearAllField}
+              role="button"
+              tabIndex={0}
+              onKeyDown={clearAllField}
+            >
+              <button type="submit" className="help-main-report-content-submit">
+                Report
+              </button>
+            </div>
+          </div>
+          <div className="help-main-report-content-description">
+            <textarea className="help-main-report-content-description-input" placeholder="Descrition" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Help;
