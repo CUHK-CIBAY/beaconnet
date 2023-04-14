1096c8c770100309e00a0f588eb4665df8b09e78
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-04-14 11:19:25 +0800

    feat:  profile page (#112)
    * feat: LikedBit-Mutation
    
    * feat: change mutation-LikedBit
    
    * feat: add searchUser-query
    
    * fix: query syntax error
    
    * fix: query syntax
    
    * other: change UI element
    
    * style: prettier
    
    * fix: fix use effect issue
    
    * feat: comment box
    
    * fix: not able to write comment when logout
    
    * fix: bug on me resolver
    
    * fix: bug
    
    * chore: admin panel
    
    ---------
    
    Co-authored-by: BillyLam1023 <billylam741987@gmail.com>
    Co-authored-by: Anson Cheng <Anson.12666@icloud.com>

--- graphql-gateway/js-graphql/src/resolvers/user/user-impl.ts
+++ graphql-gateway/js-graphql/src/resolvers/user/user-impl.ts
@@ -3,7 +3,7 @@ import driver from '../../util/neo4j-driver';
 
 dotenv.config();
 
-const userBitsResolver = async ({ id }: any) => {
+export const userBitsResolver = async ({ id }: any) => {
   const session = driver.session({ database: 'neo4j' });
   try {
     const query = 'MATCH(:User {id: $id})-[:POST]->(b:Bit) RETURN b';
@@ -17,4 +17,17 @@ const userBitsResolver = async ({ id }: any) => {
   }
 };
 
-module.exports = userBitsResolver;
+export const meResolver = async (_p: any, _a: any, { me }: any) => {
+  const session = driver.session({ database: 'neo4j' });
+  try {
+    const query = 'MATCH(u:User {id: $id}) RETURN u';
+    const result = await session.run(query, { id: me.id });
+    return result.records[0].get('u').properties;
+  } catch (error) {
+    console.error(error);
+    return null;
+  } finally {
+    await session.close();
+  }
+};
+
