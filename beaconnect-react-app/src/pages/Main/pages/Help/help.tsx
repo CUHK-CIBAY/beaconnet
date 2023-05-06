af8aac0b972dc76ae6a4f5bab5fb03be8a6bbb65
Anson Cheng <Anson.12666@icloud.com>
2023-05-07 03:25:27 +0800

    fix: help view in mobile

--- beaconnect-react-app/src/pages/Main/pages/Help/help.tsx
+++ beaconnect-react-app/src/pages/Main/pages/Help/help.tsx
@@ -10,7 +10,7 @@ const Help = () => {
   };
 
   return (
-    <div className="page-content-help">
+    <div className="page-content page-content-help">
       <div className="help-main-content-container">
         <div className="help-main-header">Help Center</div>
         <div className="help-main-description">
