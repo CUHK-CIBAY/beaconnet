e3cbbecf40fd924be0dced87ebfe8b24fa2addee
Anson Cheng <Anson.12666@icloud.com>
2023-05-12 14:38:56 +0800

    feat: register on nav bar

--- beaconnect-react-app/src/pages/Main/components/NavBar/navBar.tsx
+++ beaconnect-react-app/src/pages/Main/components/NavBar/navBar.tsx
@@ -3,41 +3,68 @@ import { AiOutlineQuestionCircle, AiOutlineSearch } from 'react-icons/ai';
 import { BiHomeAlt2 } from 'react-icons/bi';
 import { CgProfile } from 'react-icons/cg';
 import { FiLogOut, FiUser } from 'react-icons/fi';
+import { TiUserAddOutline } from 'react-icons/ti';
 import { useNavigate } from 'react-router-dom';
 import logo from './images/logo.png';
 import './nav.css';
 
-function NavItem({ icon, text, path }: { icon: JSX.Element; text: string; path: string }) {
+function NavItem({
+  icon,
+  text,
+  path,
+  desktopOnly,
+}: {
+  icon: JSX.Element;
+  text: string;
+  path: string;
+  desktopOnly?: boolean;
+}) {
   const navigate = useNavigate();
   const redirectTo = (redirectPath: string) => () => {
     navigate(redirectPath);
   };
 
   return (
-    <div className="nav-link" onClick={redirectTo(path)} onKeyDown={redirectTo(path)} role="button" tabIndex={0}>
+    <div
+      className={`nav-link ${desktopOnly ? 'desktopOnly' : ''}`}
+      onClick={redirectTo(path)}
+      onKeyDown={redirectTo(path)}
+      role="button"
+      tabIndex={0}
+    >
       {icon}
       <p className="nav-link-desc">{text}</p>
     </div>
   );
 }
 
+NavItem.defaultProps = {
+  desktopOnly: false,
+};
+
 function NavBar({ isLoggedIn }: { isLoggedIn: boolean }) {
-  let NavLinks: { icon: JSX.Element; text: string; path: string }[] = [];
-  let NavControls: { icon: JSX.Element; text: string; path: string }[] = [];
+  let NavLinks: {
+    icon: JSX.Element;
+    text: string;
+    path: string;
+    desktopOnly?: boolean;
+  }[] = [];
+  let NavControls: {
+    icon: JSX.Element;
+    text: string;
+    path: string;
+    desktopOnly?: boolean;
+  }[] = [];
   if (isLoggedIn) {
     NavLinks = [
       { icon: <BiHomeAlt2 />, text: 'Home', path: '/' },
       { icon: <CgProfile />, text: 'Profile', path: 'profile' },
-      // { icon: <AiOutlineMessage />, text: 'Message', path: 'message' },
       { icon: <AiOutlineSearch />, text: 'Search', path: 'search' },
       { icon: <AiOutlineQuestionCircle />, text: 'Help', path: 'help' },
       // { icon: <AiOutlineSetting />, text: 'Settings', path: 'settings' },
     ];
 
-    NavControls = [
-      { icon: <FiLogOut />, text: 'Logout', path: 'logout' },
-      // { icon: <FiUser />, text: 'Account', path: 'account' },
-    ];
+    NavControls = [{ icon: <FiLogOut />, text: 'Logout', path: 'logout' }];
   } else {
     NavLinks = [
       { icon: <BiHomeAlt2 />, text: 'Home', path: '/' },
@@ -49,6 +76,12 @@ function NavBar({ isLoggedIn }: { isLoggedIn: boolean }) {
         icon: <FiUser />,
         text: 'Login',
         path: 'login',
+      },
+      {
+        icon: <TiUserAddOutline />,
+        text: 'Register',
+        path: 'Register',
+        desktopOnly: true,
       },
     ];
   }
@@ -64,12 +97,24 @@ function NavBar({ isLoggedIn }: { isLoggedIn: boolean }) {
 
         <div className="nav-links">
           {NavLinks.map((link) => (
-            <NavItem icon={link.icon} text={link.text} path={link.path} key={link.text} />
+            <NavItem
+              icon={link.icon}
+              text={link.text}
+              path={link.path}
+              key={link.text}
+              desktopOnly={link.desktopOnly}
+            />
           ))}
         </div>
         <div className="nav-controls">
           {NavControls.map((link) => (
-            <NavItem icon={link.icon} text={link.text} path={link.path} key={link.text} />
+            <NavItem
+              icon={link.icon}
+              text={link.text}
+              path={link.path}
+              key={link.text}
+              desktopOnly={link.desktopOnly}
+            />
           ))}
         </div>
       </div>
